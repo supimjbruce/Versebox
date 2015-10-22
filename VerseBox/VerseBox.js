72fdef4cbda83b7862ec5b864fb629406053868a
JwBZx <Jwbmw3@gmail.com>
2015-10-21 20:16:08 -0400

    VerseBox App (JavaScript File)

---
+++ VerseBox/VerseBox.js
@@ -0,0 +1,49 @@
+console.log("Welcome to VerseBox: My JWB Bible Verse Storage");
+console.log("By @JwBZx");
+console.log("(You can view this Project on GitHub at github.com/JwBZx/VerseBox)");
+// I hope that you like this Project! Hopefully my accountability partner Justin will like it as well haha :P
+var justinVerses = false;
+var sevenNineVerses = false;
+// Introduction:
+console.log("Welcome to VerseBox! This a simple JavaScript Application that stores all of the Bible Verses I have in my real life Verse Box!");
+console.log("In this app, you can view the Verses my accountability partner Justin has given me, or you can view the verses I have gotton from 7-9, a Bible Study group I am in!");
+console.log("If you want to view my verses from Justin, type 'JUSTIN' in the prompt box below.");
+console.log("If you want to view my verses from 7-9, type 'SEVENNINE' in the prompt box below.");
+console.log("Thank You for checking out this application! I hope you like it!");
+console.log("Thank You and God Bless!");
+console.log("---Jonathan Bruce (@JwBZx)");
+// The Starting Block (Prompts):
+prompt("So, what verses do you want to view? Type either 'JUSTIN' or 'SEVENNINE'");
+if(prompt = "justin".toUppercase) {
+	var justinVerses = true;
+	console.log("Okay sweet! Let's look at the verses I got from Justin!");
+} else if(prompt = "sevennine".toUppercase) {
+	var sevenNineVerses = true;
+	console.log("Okay sweet! Let's look at the verses I got from 7-9!");
+}
+// My Justin Verses:
+console.log("Here are the verses I have gotton from Justin: [To view the full verse, type the verse name as instructed below]");
+console.log("Verse");
+console.log("Verse");
+console.log("Verse");
+console.log("Verse");
+console.log("Verse");
+console.log("Verse");
+prompt("So which verse do you want to view? Type the word inside of the brackets to view that particular verse.");
+if(prompt =
+
+// My 7-9 Verses:
+console.log("Here are the verses I have gotton from 7-9: [To view the full verse, type the verse name as instructed below]");
+console.log("Verse");
+console.log("Verse");
+console.log("Verse");
+console.log("Verse");
+prompt("So which verse do you want to view? Type the word inside of the brackets to view that particular verse.");
+
+
+// The Finish Line (End):
+console.log("Well, that is the end of this app! I hope you liked it!");
+console.log("You can view this project on GitHub at github.com/JwBZx/VerseBox");
+console.log("If you want to offer any suggestions and comments, you can open an issue on this project on GitHub.");
+console.log("Thank You and God Bless!");
+console.log("---Jonathan Bruce (@JwBZx)");
