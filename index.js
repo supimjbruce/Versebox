5a4e6c56eec59d616479746dc60c331d9438f76b
Jonathan Bruce <supimjbruce@gmail.com>
2025-08-31 23:06:40 -0400

    Updated index.js
    Updated index.js tonight! Tried to review some of my code notes and start working on adding in some props for the Bible verses and the Verse cards! Going to keep working on this project! Goodnight GitHub!
    
    .j.

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
+/* VerseBox */
+/* Home Page [index.js] */
+
+/*By Jonathan Bruce
+JBruce, LLC
+jbruce.design
+.j.
+- - - - - */
+
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 export default function VerseBoxHomeScreen() {
+
+  const BibleVerse = ({verse}) => {
+    return (
+      <View>
+        <Text>{verse}</Text>
+      </View>
+    );
+  };
+
+  const StackOfVerses = () => {
+    return (
+      <View>
+        <BibleVerse verse="2 Peter 1:3" />
+        <BibleVerse verse="John 3:16" />
+      </View>
+    );
+  };
+
     return (
         <View style={styles.container}>
-            <View style={styles.middleTitle}>
-                <Text style={styles.header}>VerseBox</Text>
-                <Text style={styles.subTitle}>A simple application for<br></br>storing your Bible verses</Text>
+            <View style={styles.verseBox}>
+              <StackOfVerses />
             </View>
             <View style={styles.footer}>
-                <Text style={styles.footerText}>Made by JBruce</Text>
-                <Text style={styles.footerText}>@supimjbruce</Text>
+              <View /*style={styles.middleTitle}*/>
+                  <Text style={styles.header}>VerseBox</Text>
+                  <Text style={styles.subTitle}>A simple application for<br></br>storing your Bible verses</Text>
+              </View>
+              <View>
+                  <Text style={styles.footerText}>Made by JBruce</Text>
+                  <Text style={styles.footerText}>@supimjbruce</Text>
+              </View>
             </View>
         </View>
     )
@@ -18,15 +50,21 @@ export default function VerseBoxHomeScreen() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#3396D3',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#3396D3',
     },
 
-    middleTitle: {
-      flex: 1,
+    /*middleTitle: {
       justifyContent: 'center'
+    },*/
+
+    verseBox: {
+      height: '40%',
+      width: '80%',
+      backgroundColor: '#C6D870',
+      marginBottom: 25,
     },
 
     header: {
